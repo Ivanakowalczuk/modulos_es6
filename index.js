05153aa14407d0743a09e8cb0be05072426ce5b0
Ivanakowalczuk <ivanakowalczuk@gmail.com>
2022-06-05 21:30:02 -0300

    modulos y librerías

---
+++ index.js
@@ -0,0 +1,10 @@
+ import {suma, multiplica} from './modulos/controller.js'
+ import chalk from 'chalk';
+ 
+
+ const mult = multiplica(1,2)
+ const sum = suma(4,5)
+ 
+ 
+ console.log(mult)
+ console.log(chalk.green(sum))
---
+++ modulos/controller.js
@@ -0,0 +1,21 @@
+export function suma (a, b){
+    return a + b;
+}
+
+function resta (a, b){
+    return a - b;
+}
+
+export function multiplica (a, b){
+    return a * b;
+}
+
+function divide (a, b){
+    return a/b;
+}
+
+function potencia (a, b){
+    return a ** b;
+}
+
+
